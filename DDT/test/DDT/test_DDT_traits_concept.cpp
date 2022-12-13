52565257c287e2070fdda27a5cf3ba8df801a80a
Sébastien Loriot <sebastien.loriot@cgal.org>
2022-12-13 16:03:06 +0100

    do not run concept check

--- DDT/test/DDT/test_DDT_traits_concept.cpp
+++ DDT/test/DDT/test_DDT_traits_concept.cpp
@@ -1,7 +1,11 @@
 #include "../../../DDT/doc/DDT/Concepts/TriangulationTraits.h"
 #include "test_traits.h"
 
-int main(int, char **)
+int main(int argc, char **)
 {
-    return test_traits<::TriangulationTraits>("test_DDT_traits_2_out", 3, 1000);
+  // do not run it!
+  if (argc == 33)
+    test_traits<::TriangulationTraits>("test_DDT_traits_2_out", 3, 1000);
+
+  return 0;
 }
